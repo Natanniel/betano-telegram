fd463903d024fc8562cc0bdf585581b165e04931
Natanniel <natanniel.alves@outlook.com>
2022-06-08 16:55:01 -0300

    deploy

--- dados.py
+++ dados.py
@@ -1,12 +1,61 @@
-from pymongo import ASCENDING, MongoClient
+from pymongo import ASCENDING, DESCENDING, MongoClient
 CONNECTION_STRING = "mongodb+srv://cassino:cassino@cluster0.rjggj.mongodb.net/betano?retryWrites=true&w=majority"
 
-def SelecionaTodosSinais(tipo):
+def SelecionaTodasRoletas():
+    
+
+    client = MongoClient(CONNECTION_STRING)
+    db =  client['betano']
+    roletas = db.get_collection('roletas')
+    dados = roletas.find()    
+    return dados
+
+
+def SelecionaTodosSinais(roleta):
     client = MongoClient(CONNECTION_STRING)
     db =  client['betano']
     registros = db.get_collection('registros')
-    return registros.find({'type' : tipo}).sort([('created_at', ASCENDING)])
+    return registros.find({'roulette' : roleta}).sort([('created_at', DESCENDING)]).limit(30)
 
+
+
+
+def SelecionaEstrategias():
+    client = MongoClient(CONNECTION_STRING)
+    db =  client['betano']
+    registros = db.get_collection('estrategias')
+    return registros.find({})
+
+
+def SelecionaSinalExistente(tipo,nomeRoleta):
+    client = MongoClient(CONNECTION_STRING)
+    db =  client['betano']
+    registros = db.get_collection('sinal')
+    return registros.find({'tipo':tipo , 'roleta' : nomeRoleta, 'ativo': 1 })
+
+
+def inserirSinal(tipo,nomeRoleta):
+    client = MongoClient(CONNECTION_STRING)
+    db = client['betano']
+    sinais = db.get_collection('sinal')
+    sinais.insert_one({'tipo':tipo , 'roleta' : nomeRoleta, 'ativo': 1, 'status': 1, 'jogadas':0 })
+
+def confirmaSinal(tipo,nomeRoleta,jogadas):
+    client = MongoClient(CONNECTION_STRING)
+    db = client['betano']
+    sinais = db.get_collection('sinal')
+    sinais.update_one({'tipo':tipo , 'roleta' : nomeRoleta.replace('-',' ') },  {"$set": {'status': 2, 'jogadas': jogadas}} )
+
+
+def resultadoSinal(tipo,nomeRoleta,resultado):
+    client = MongoClient(CONNECTION_STRING)
+    db = client['betano']
+    sinais = db.get_collection('sinal')
+    sinais.update_one({'tipo':tipo , 'roleta' : nomeRoleta.replace('-',' ') },  {"$set": {'ativo': 2, 'status' : resultado }} )
+    
+    db2 = client['betano']    
+    registros = db2.get_collection('registros')
+    registros.delete_many({"roulette": nomeRoleta })
 
 def selecionaTodosClientes():
     client = MongoClient(CONNECTION_STRING)
--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+
 import time
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup,ReplyKeyboardMarkup 
 from telegram.ext import CallbackQueryHandler
@@ -8,23 +9,216 @@ from telegram.ext.commandhandler import CommandHandler #Esta classe Handler é u
 from telegram.ext.messagehandler import MessageHandler #Esta classe Handler é usada para lidar com qualquer mensagem normal enviada pelo usuário ao bot
 from telegram.ext.filters import Filters # Isso filtrará texto normal, comandos, imagens, etc. de uma mensagem enviada.
 
-from dados import inserirNovoUsuario
+from dados import resultadoSinal,inserirSinal,confirmaSinal,SelecionaSinalExistente,inserirNovoUsuario,SelecionaTodasRoletas,SelecionaEstrategias, SelecionaTodosSinais
+from src.estrategia import encontrarEstrategia
 
-print('Telegram bot inicialiado')
+print('Telegram bot inicializado')
 
-updater = Updater("5350481212:AAEpiE5l-qYkEommV2AvQ5oBUS1-qt9jqSQ", use_context=True)
+telegram = Updater("5350481212:AAEpiE5l-qYkEommV2AvQ5oBUS1-qt9jqSQ", use_context=True)
+
+roletas = []
+
+def iniciar():   
+    mensagem = "*Seja bem-vindo(a)*\n\n" 
+    mensagem = "*ATENÇÂO 🚨*\n\n___O Robo esta iniciando a analise das roletas___\n\n"
+    #telegram.bot.send_message(782375549,mensagem,parse_mode= 'Markdown')
+  
+    sinaisEmAndamento = []
+
+    while(True):
+
+        tiposRoletas = SelecionaTodasRoletas()
+        
+        # Pega todos os tipos de roletas
+        for roletas in tiposRoletas:
+
+            # pega todas as roletas
+            for roleta in roletas['roletas']:
+
+                #iniciar captura dos sinais e analisa a roleta
+                roleta = roleta['nome'].replace(' ','-')
+                sinais = SelecionaTodosSinais(roleta)
+
+                interromper = False
+                sinaisTratados = []
+
+                for sinal in sinais:
+                    if(interromper == False):
+                        try:
+                            sinaisTratados.append(sinal['value'])
+                        except:
+                            interromper = True
+                            resultadoSinal(roletas['nome'],roleta, 5 )
+                
+                  
+                bolas = [0,0,0,0,0,0,0,0,0,0,0,0]
+                
+                for sinal in sinaisTratados:
+                    bolas = encontrarEstrategia(sinal,bolas)
+
+
+                estrategias = SelecionaEstrategias()
+
+                # LEGENDA DO ARRAY DE BOLAS
+                # 0 - Vermelho | 1 - Preto | 2 - Impar
+                # 3 - Par | 4 - Alto | 5 - Baixo
+                # 6 - Duzia 1 | 7 - Duzia 2 | 8 - Duzia 3
+                # 9 - Coluna 1 | 10 - Coluna 2 | Coluna 3
+
+                # Analisa se bate com alguma estrategia =============================
+                print(bolas)
+
+                # Verificar se ja existe algum sinal em andamento 
+                sinalEmAndamento = SelecionaSinalExistente(roletas['nome'],roleta.replace('-',' '))
+                status = 0
+                jogadasAntiga = 0
+
+                for sinalAndamento in sinalEmAndamento:
+                    status = sinalAndamento['status']
+                    if status == 2:
+                        jogadasAntiga = sinalAndamento['jogadas']
+                
+                ## ANALISA OS SINAIS ======================
+                if status == 0:
+                    analisaMesa(roletas['nome'],estrategias,bolas,roleta)
+
+                ## COMFIRMA ENTRADA ======================
+                if status == 1:
+                    confirmaEntrada(roletas['nome'],estrategias,bolas,roleta,len(sinaisTratados))
+
+                ## VERIFICA WIN x LOSS ========================
+                if status == 2:
+                    confirmaWin(roletas['nome'],estrategias,bolas,roleta,len(sinaisTratados),jogadasAntiga)
+
+               
+        time.sleep(10)
+        #print(roletas)
+        
+
+
+def confirmaWin(tipo,estrategias,bolas,roleta,jogadas,jogadasAntiga):
+
+    if(jogadas > jogadasAntiga):
+
+        loss = True
+
+        for estrategia in estrategias:
+            
+            # REPETICAO
+            if(estrategia['estrategia'].split('-')[0] == 'r'):
+
+                ## REPETINDO VERMELHO ===========================
+                if(estrategia['estrategia'].split('-')[1] == 'v'):
+                    
+                    ## WIN OU LOSS
+                    if(estrategia['confirma'] < bolas[0]):
+                        resultadoSinal(tipo,roleta, 3)
+                        loss = False
+                 
+                    
+
+                ## REPETINDO PRETO ===========================
+                if(estrategia['estrategia'].split('-')[1] == 'p'):
+                 
+                  ## WIN OU LOSS
+                    if(estrategia['confirma'] < bolas[1]):
+                        resultadoSinal(tipo,roleta, 3 )
+                        loss = False
+                    
+        if loss == True:
+            resultadoSinal(tipo,roleta, 4 )
+
+
+def confirmaEntrada(tipo,estrategias,bolas,roleta,jogadas):
+    
+    for estrategia in estrategias:
+        
+        # REPETICAO
+        if(estrategia['estrategia'].split('-')[0] == 'r'):
+
+            ## REPETINDO VERMELHO ===========================
+            if(estrategia['estrategia'].split('-')[1] == 'v'):
+                ## ANALISANDO REPETICAO DO VERMELHO
+                if(estrategia['confirma'] == bolas[0]):
+                    confirmado(tipo,roleta, 'Repeticao do vermelho',estrategia['aposta'],jogadas)
+
+            ## REPETINDO PRETO ===========================
+            if(estrategia['estrategia'].split('-')[1] == 'p'):
+                ## ANALISANDO REPETICAO DO VERMELHO
+               # if(estrategia['confirma'] == bolas[1]):
+                if(3 == bolas[1]):
+                    confirmado(tipo,roleta, 'Repeticao do preto',estrategia['aposta'],jogadas)
+
+
+
+def analisaMesa(tipo,estrategias,bolas,roleta):
+    
+    for estrategia in estrategias:
+        
+        # REPETICAO
+        if(estrategia['estrategia'].split('-')[0] == 'r'):
+
+            ## REPETINDO VERMELHO ===========================
+            if(estrategia['estrategia'].split('-')[1] == 'v'):
+                ## ANALISANDO REPETICAO DO VERMELHO
+                if(estrategia['analise'] == bolas[0]):
+                    analisando(roletas['nome'],roleta, 'Repeticao do vermelho')
+
+            ## REPETINDO PRETO ===========================
+            if(estrategia['estrategia'].split('-')[1] == 'p'):
+                ## ANALISANDO REPETICAO DO VERMELHO
+                if(estrategia['analise'] == bolas[1]):
+                    analisando(tipo,roleta, 'Repeticao do preto')
+          
+        
+
+
+
+
+def analisando(tipo,roleta,estrategia):
+    mensagem = "⏱️ *ANALISANDO MESA*\n\n"
+    mensagem += "🎰 *ROLETA* ___"+roleta.replace('-',' ')+"___\n"
+    mensagem += "♟ *ESTRATEGIA* ___"+estrategia+"___"
+    telegram.bot.send_message(782375549,mensagem,parse_mode= 'Markdown' )
+    inserirSinal(tipo,roleta.replace('-',' '))
+
+
+     
+   
+def confirmado(tipo,roleta,estrategia,aposta,jogadas): 
+   
+    entrada = ""
+
+    if(aposta == "a-v"):
+        entrada = "Apostar no vermelho"
+
+    if(aposta == "a-p"):
+        entrada = "Apostar no preto"
+   
+   
+    mensagem = "✅ *JOGADA CONFIRMADA*\n\n"
+    mensagem += "🎰 *ROLETA* ___"+roleta.replace('-',' ')+"___\n"
+    mensagem += "♟ *ESTRATEGIA* ___"+estrategia+"___\n"
+    mensagem += "⚡️*ENTRADA*: ___" + entrada +"___"
+    telegram.bot.send_message(782375549,mensagem,parse_mode= 'Markdown' )
+    confirmaSinal(tipo,roleta,jogadas)
+
+
+iniciar()
+
 
 # inicializar o processo do BOT
-def start(update: Update, context: CallbackContext):
-    mensagem = "*Seja bem-vindo(a)*\n\n"
-    update.message.reply_text(mensagem,parse_mode= 'Markdown' )
-    mensagem = "*ATENÇÂO 🚨*\n\n___Projeto em estagio de criação___\n\n"
-    update.message.reply_text(mensagem,parse_mode= 'Markdown' )
-    inserirNovoUsuario(update.message.chat_id) # Cadastrar novo usuario
+#def start(update: Update, context: CallbackContext):
+    
+    #
+    #update.message.reply_text(mensagem,parse_mode= 'Markdown' )
+    #mensagem = "*ATENÇÂO 🚨*\n\n___Projeto em estagio de criação___\n\n"
+    #update.message.reply_text(mensagem,parse_mode= 'Markdown' )
+    #inserirNovoUsuario(update.message.chat_id) # Cadastrar novo usuario
 
-
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.start_polling()
+##
+##updater.dispatcher.add_handler(CommandHandler('start', start))
+##updater.start_polling()
 
 
 
